4ed26da434503b5c5673a83f2d07c37f3d0c4988
charlie <charles.tuchlinsky@outlook.com>
2020-06-11 09:31:59 -0400

    test

---
+++ minecraft.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#read -p 'server name: ' name
+server_name=$1
+output=$(screen -ls)
+if [[ "$output" =~ "$server_name" ]]
+then
+    echo 'server is running'
+else
+    screen -dmS $server_name
+    echo starting $server_name
+    screen -S $server_name -X stuff 'cd /var/opt/minecraft/crafty/servers/'$server_name/''$(echo -ne '\015')
+    screen -S $server_name -X stuff '/var/opt/minecraft/crafty/servers/'$server_name/'start.sh'$(echo -ne '\015')
+    screen -ls | grep $server_name
+fi
