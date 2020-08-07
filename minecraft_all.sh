e6ae5fe01adf1f2ddabdcb0e5babde9eee1b2517
shadows4321 <charles.tuchlinsky@outlook.com>
2020-08-06 20:01:03 -0400

    updating

--- minecraft_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./minecraft.sh waterfall
-./minecraft.sh lobby
-./minecraft.sh pvp
-./minecraft.sh abby
-./minecraft.sh zach
-./minecraft.sh factions
-./minecraft.sh kenny
-./minecraft.sh space\ x
